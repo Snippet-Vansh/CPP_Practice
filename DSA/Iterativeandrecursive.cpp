5a4103a04194660cfa03e47a8e520807acc2926f
Ezzzz-Vansh🦖 <vanshvishwakarma1514@gmail.com>
2026-06-26 23:48:54 +0530

    Add files via upload

---
+++ DSA/Iterativeandrecursive.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int> &arr, int target)
+{
+    int l = 0, r = arr.size() - 1;
+    while (l <= r)
+    {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            l = mid + 1;
+        else
+            r = mid - 1;
+    }
+    return -1;
+}
