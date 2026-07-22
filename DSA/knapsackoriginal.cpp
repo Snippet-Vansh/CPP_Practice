cf5fe90a20ff17fa413fc8f882658bf99223223e
Ezzzz-Vansh🦖 <vanshvishwakarma1514@gmail.com>
2026-07-23 01:25:52 +0530

    Add files via upload

---
+++ DSA/knapsackoriginal.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int knapsack(int W, int wt[], int val[], int n)
+{
+    vector<vector<int>> K(n + 1, vector<int>(W + 1));
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int w = 0; w <= W; w++)
+        {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]],
+                              K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+
+    return K[n][W];
+}
